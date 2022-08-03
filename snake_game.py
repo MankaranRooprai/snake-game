180106cfca9773abc97fbb16704526ff6e0b3254
Mankaran Rooprai <mankaranrooprai@gmail.com>
2022-08-02 22:46:53 -0400

    Displayed a 'you lost' sign when the snake leaves or touches the boundary.

--- snake_game.py
+++ snake_game.py
@@ -1,6 +1,7 @@
 import pygame
-
+import time
 pygame.init()
+
 dis = pygame.display.set_mode((400, 300))
 pygame.display.update()
 pygame.display.set_caption('Snake game by Mankaran')
@@ -13,13 +14,22 @@ dis = pygame.display.set_mode((800, 600))
 
 game_over = False
 
-x1 = 300
+x1 = 400
 y1 = 300
+
+snake_block = 10
 
 x1_change = 0
 y1_change = 0
 
 clock = pygame.time.Clock()
+snake_speed = 30
+
+font_style = pygame.font.SysFont(None, 50)
+
+def message(msg, color):
+	mesg = font_style.render(msg, True, color)
+	dis.blit(mesg, [400, 300])
 
 while not game_over:
 	for event in pygame.event.get():
@@ -40,15 +50,22 @@ while not game_over:
 				y1_change = 10
 				x1_change = 0
 
+	if x1 >= 800 or x1 < 0 or y1 >= 600 or y1 < 0:
+		game_over = True
+
 	x1 += x1_change
 	y1 += y1_change
 
 	dis.fill(white)
-	pygame.draw.rect(dis, black, [x1, y1, 10, 10])
+	pygame.draw.rect(dis, black, [x1, y1, 10, 10]) #x and y coordinates, and width and height of rectangle
 
 	pygame.display.update()
 
 	clock.tick(30)
+
+message("You lost", red)
+pygame.display.update()
+time.sleep(2)
 
 pygame.quit()
 quit()
